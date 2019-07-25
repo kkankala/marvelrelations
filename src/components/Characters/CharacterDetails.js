386bbf831c361a892503afe0381a2693acfabe09
Kalyan Kankala <kankala.kalyan@gmail.com>
2019-07-24 21:46:42 -0400

    char events, refactor required. bug in ajax calls

--- src/components/Characters/CharacterDetails.js
+++ src/components/Characters/CharacterDetails.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { fetchGetApi } from '../../api/apiUtils';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
-import CharacterItem from './CharacterItem';
+import { Grid, Paper, Typography, Divider, Chip } from '@material-ui/core';
+import CharacterItemInfo from './CharacterItemInfo';
 
 const useStyles = makeStyles(() => ({
   root: {
-    marginTop: '64px'
+    //marginTop: '64px'
   },
   gridList: {
     minWidth: 500,
     minHeight: 500
   },
-  card: {
-    maxWidth: 345,
-    margin: 5
-  },
+  // card: {
+  //   maxWidth: 345,
+  //   margin: 5
+  // },
   cardActions: {
     justifyContent: 'flex-end'
   }
@@ -24,26 +24,23 @@ const useStyles = makeStyles(() => ({
 export function CharacterDetails({ match, ...props }) {
   const [events, setEvents] = useState([]);
   const classes = useStyles();
-  const [characterItemData, setCharacterItemData] = useState({});
+  const [characterItemData, setCharacterItemData] = useState({
+    events: { items: [] }
+  });
   useEffect(() => {
-    const fetchEventsData = async () => {
-      const result = await fetchGetApi(
-        `characters/${match.params.charId}/events`
-      );
-      setEvents(result.data.results);
-    };
     const fetchCharItemData = async () => {
       const result = await fetchGetApi('/characters/' + match.params.charId);
+      console.log(result.data.results[0].events.items[0].name);
       setCharacterItemData(result.data.results[0]);
+      setEvents(characterItemData.events.items);
     };
-    fetchCharItemData().then(fetchEventsData());
-  }, [match.params.charId]);
+    fetchCharItemData();
+  }, [characterItemData.events.items, match.params.charId]);
 
   return (
     <div className={classes.root}>
       <Grid
         container
-        spacing={10}
         direction="row"
         justify="center"
         alignItems="center"
@@ -51,14 +48,26 @@ export function CharacterDetails({ match, ...props }) {
       >
         <Grid item xs={6}>
           {characterItemData.id && (
-            <CharacterItem
+            <CharacterItemInfo
               key={characterItemData.id}
               charItem={characterItemData}
             />
           )}
         </Grid>
         <Grid item xs={6}>
-          {/* <Paper className={classes.paper}>xs=6</Paper> */}
+          <Paper>
+            <Typography variant="h5" gutterBottom>
+              Events related to {characterItemData.name}
+            </Typography>
+            <Divider />
+            {characterItemData.events.items.map(item => (
+              <Chip
+                key={item.name}
+                label={item.name}
+                className={classes.chip}
+              />
+            ))}
+          </Paper>
         </Grid>
       </Grid>
     </div>
--- src/components/Characters/CharacterItemInfo.js
+++ src/components/Characters/CharacterItemInfo.js
@@ -6,55 +6,98 @@ import {
   CardMedia,
   CardContent,
   Typography,
-  IconButton
+  IconButton,
+  Collapse
 } from '@material-ui/core';
 
 import IconLink from '@material-ui/icons/Link';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 
-import React from 'react';
-
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   card: {
-    minWidth: 345,
+    // minWidth: 500,
+    maxWidth: 800,
+    // minHeight: 500,
+    // maxHeight: 650,
     margin: 5
   },
   cardActions: {
-    // justifyContent: 'flex-end'
+    justifyContent: 'flex-end'
+  },
+  //media: {
+  // height: 0,
+  // paddingTop: '56.25%' // 16:9
+  //},
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
   }
 }));
 
-export default function CharacterItem({ charItem, ...props }) {
+export default function CharacterItemInfo({ charItem, ...props }) {
   const classes = useStyles();
-
+  const [expanded, setExpanded] = useState(false);
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
   return (
-    <Card className={classes.card} key={charItem.id}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt={charItem.name}
-          height="140"
-          image={charItem.thumbnail.path + '.' + charItem.thumbnail.extension}
-          title={charItem.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {charItem.name}
-          </Typography>
-          {/* <Typography variant="body2" color="textSecondary" component="p">
-                  {item.description}
-                </Typography> */}
-        </CardContent>
-      </CardActionArea>
-      <CardActions className={classes.cardActions}>
-        <IconButton
-          aria-label="Upload picture"
-          component="a"
-          target="_blank"
-          href={charItem.urls[0].url}
-        >
-          <IconLink />
-        </IconButton>
-      </CardActions>
-    </Card>
+    charItem && (
+      <Card className={classes.card} key={charItem.id}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={charItem.name}
+            image={charItem.thumbnail.path + '.' + charItem.thumbnail.extension}
+            title={charItem.name}
+            className={classes.media}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {charItem.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {charItem.comics.available} Comics, {charItem.series.available}{' '}
+              Series and {charItem.stories.available} Stories
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions className={classes.cardActions}>
+          <IconButton
+            aria-label="Upload picture"
+            component="a"
+            target="_blank"
+            href={charItem.urls[0].url}
+          >
+            <IconLink />
+          </IconButton>
+          {charItem.description.length > 0 && (
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          )}
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Description</Typography>
+            <Typography paragraph>{charItem.description}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    )
   );
 }
